7e56451bf8f05d08846b78c73503486898b849c3
akbarshohjahonov <akbarshohjahonov@akbarshohjahonov@gmail.com>
2022-11-29 15:35:50 +0500

    akskas

--- build_func03.py
+++ build_func03.py
@@ -1,2 +1,6 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func03
+import math
+n=3.5
+answer=3*(pow(n+1,2))
+print(answer)
